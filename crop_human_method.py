fed93fbf7a1e908755d6cb3cd2298813b6d25087
T.Aathman <aathmant@gmail.com>
2020-09-14 19:50:58 +0530

    fix

--- crop_human_method.py
+++ crop_human_method.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 cfg_file = '/content/ThEmoBe_basic/CVND_Exercises_2_2_YOLO/cfg/yolov3.cfg'
 
 # Set the location and name of the pre-trained weights file
-weight_file = '/content/ThEmoBe_basic/yolov3.weights'
+weight_file = '/content/drive/My Drive/yolov3.weights'
 
 # Set the location and name of the COCO object classes file
 namesfile = '/content/ThEmoBe_basic/CVND_Exercises_2_2_YOLO/data/coco.names'
