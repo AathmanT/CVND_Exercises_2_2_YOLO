fc72d4fdc5399672ff6f94a92bd5d5594aefa85e
T.Aathman <aathmant@gmail.com>
2020-11-04 15:22:23 +0530

    remove printing of  "It took _ seconds to detect objects and Number of Objects detected"

--- utils.py
+++ utils.py
@@ -130,10 +130,10 @@ def detect_objects(model, img, iou_thresh, nms_thresh):
     finish = time.time()
     
     # Print the time it took to detect objects
-    print('\n\nIt took {:.3f}'.format(finish - start), 'seconds to detect the objects in the image.\n')
+    # print('\n\nIt took {:.3f}'.format(finish - start), 'seconds to detect the objects in the image.\n')
     
     # Print the number of objects detected
-    print('Number of Objects Detected:', len(boxes), '\n')
+    # print('Number of Objects Detected:', len(boxes), '\n')
     
     return boxes
 
